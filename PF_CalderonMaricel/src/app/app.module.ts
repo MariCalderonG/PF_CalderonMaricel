13226770be776e26bbd59fd674a7a32a75ca1178
Maricel Calderon <maricalderon424@gmail.com>
2022-05-26 15:31:39 -0300

    Correciones: eliminación de socialmedia, acomodo header

--- PF_CalderonMaricel/src/app/app.module.ts
+++ PF_CalderonMaricel/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArgProgComponent } from './components/logo-arg-prog/logo-arg-prog.component';
-import { SocialmediaComponent } from './components/socialmedia/socialmedia.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -20,7 +19,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoArgProgComponent,
-    SocialmediaComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
