49dfcc7a34d75d305e97c6a5fb126ece4a0d797f
Maricel Calderon <maricalderon424@gmail.com>
2022-07-03 12:15:46 -0300

    Creación carpeta BackEnd

---
+++ PF_CalderonMaricel/BackEnd/src/main/java/com/portfolio/mcg/McgApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.mcg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McgApplication.class, args);
+	}
+
+}
---
+++ PF_CalderonMaricel/BackEnd/src/test/java/com/portfolio/mcg/McgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.mcg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
